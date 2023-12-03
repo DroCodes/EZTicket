59bb7936082e49668bc91b717f4a2947bd571d1a
Deon Daigh <deon.daigh@gmail.com>
2023-12-03 12:19:48 -0600

    Add admin functionality, can now delete tickets and users

--- EZTicket/EZTicket/Models/UserModel.cs
+++ EZTicket/EZTicket/Models/UserModel.cs
@@ -4,5 +4,8 @@ namespace EZTicket.Models;
 
 public class UserModel : IdentityUser
 {
-    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string PhoneNumber { get; set; }
 }
