fd1594fb7fd2768663ad5c3466fb827b73df2b33
Deon Daigh <deon.daigh@gmail.com>
2023-12-02 19:56:41 -0600

    Added Identity server, can not log in and create users.

--- EZTicket/EZTicket/Models/RegisterModel.cs
+++ EZTicket/EZTicket/Models/RegisterModel.cs
@@ -6,4 +6,5 @@ public class RegisterModel
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
 }
